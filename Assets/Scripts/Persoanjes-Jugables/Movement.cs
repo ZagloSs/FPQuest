71e9603476fab884fe4301933a406025471449d8
Marcosino4 <marcosino654@gmail.com>
2024-04-09 20:16:42 +0200

    Animacion caminar arriba

--- Assets/Scripts/Persoanjes-Jugables/Movement.cs
+++ Assets/Scripts/Persoanjes-Jugables/Movement.cs
@@ -9,12 +9,12 @@ public class Movement : MonoBehaviour
     private Rigidbody2D _rb;
     public Animator animator;
 
-    
+
 
     private void Start()
     {
-        //Pruebas 
-        animator.SetBool("IsWalkingSide", true);
+        //Pruebas
+        animator.SetBool("IsWalkingSide", false);
         //FIn Pruebas (Borrar Luego)
         _rb = GetComponent<Rigidbody2D>();
     }
@@ -22,21 +22,36 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
         _rb.velocity = new Vector2(moveX, moveY) * speed;
         if (moveX < 0)
         {
             transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetBool("IsWalkingSide", true);
+        }
+        else if(moveX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("IsWalkingSide", true);
         }
         else
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetBool("IsWalkingSide", false);
         }
 
+        if (moveY < 0)
+        {
+            animator.SetBool("IsWalkingDown", true);
+        }
+        else if (moveY > 0)
+        {
+            animator.SetBool("IsWalkingUp", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalkingDown", false);
+            animator.SetBool("IsWalkingUp", false);
+        }
     }
-
-
-        
 }
