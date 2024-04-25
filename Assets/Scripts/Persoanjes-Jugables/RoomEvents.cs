5a46e782529be1430be3d615fb33379436207b2b
Marcosino4 <marcosino654@gmail.com>
2024-04-25 14:08:42 +0200

    Script para moverse entre habitaciones

--- Assets/Scripts/Persoanjes-Jugables/RoomEvents.cs
+++ Assets/Scripts/Persoanjes-Jugables/RoomEvents.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RoomEvents : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera mainCamera;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        mainCamera = Camera.main;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Door"))
+        {
+            Debug.Log("Colision con puerta");
+            switch (collision.gameObject.name)
+            {
+                case "TopDoor":
+                    mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y + 12, mainCamera.transform.position.z);
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 5, this.transform.position.z);
+                    break;
+                case "BottomDoor":
+                    mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y - 12, mainCamera.transform.position.z);
+                    this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 5, this.transform.position.z);
+                    break;
+                case "LeftDoor":
+                    mainCamera.transform.position = new Vector3(mainCamera.transform.position.x - 20, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                    this.transform.position = new Vector3(this.transform.position.x - 5, this.transform.position.y, this.transform.position.z);
+
+                    break;
+                case "RightDoor":
+                    mainCamera.transform.position = new Vector3(mainCamera.transform.position.x + 20, mainCamera.transform.position.y, mainCamera.transform.position.z);
+                    this.transform.position = new Vector3(this.transform.position.x + 5, this.transform.position.y + 5, this.transform.position.z);
+
+                    break;
+            }
+        }
     }
 }
