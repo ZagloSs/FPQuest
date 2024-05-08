3b3d942a149c11efedc0d6634cdef55fe8d13062
ZagloSs <carlosdiezmendez@gmail.com>
2024-05-08 12:51:34 +0200

    arreglado bug animacion y tamaño sprite

--- Assets/Scripts/Persoanjes-Jugables/Movement.cs
+++ Assets/Scripts/Persoanjes-Jugables/Movement.cs
@@ -35,34 +35,38 @@ public class Movement : MonoBehaviour
         _rb.velocity = new Vector2(moveX, moveY) * GetComponent<Properties>().Speed;
         if (moveX < 0)
         {
-            transform.localScale = new Vector3(-1, 1, 1);
+            GetComponent<SpriteRenderer>().flipX = true;
             animator.SetBool("IsWalkingSide", true);
             lastMovedVector = new Vector2(moveDir.x, 0f);
         }
         else if(moveX > 0)
         {
-            transform.localScale = new Vector3(1, 1, 1);
+            GetComponent<SpriteRenderer>().flipX = false;
             animator.SetBool("IsWalkingSide", true);
             lastMovedVector = new Vector2(moveDir.x, 0f);
         }
         else
         {
+           
             animator.SetBool("IsWalkingSide", false);
             lastMovedVector = new Vector2(moveDir.x , moveDir.y);
         }
 
         if (moveY < 0)
         {
+           
             animator.SetBool("IsWalkingDown", true);
             lastMovedVector = new Vector2(0f, moveDir.y);
         }
         else if (moveY > 0)
         {
+            
             animator.SetBool("IsWalkingUp", true);
             lastMovedVector = new Vector2(0f, moveDir.y);
         }
         else
         {
+            
             animator.SetBool("IsWalkingDown", false);
             animator.SetBool("IsWalkingUp", false);
             lastMovedVector = new Vector2(moveDir.x, moveDir.y);
