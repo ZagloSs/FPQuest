2933892a12967310a64718aec5093385784a8521
Carlos Díez <71779031+ZagloSs@users.noreply.github.com>
2024-05-13 09:57:20 +0200

    Añadido Comentarios AltarItem.cs

--- Assets/Scripts/Rooms/AltarItem.cs
+++ Assets/Scripts/Rooms/AltarItem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AltarItem : MonoBehaviour
 {
-
+    //Utilizado para cambiar el estado del pedestal una vez recogido el item
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
