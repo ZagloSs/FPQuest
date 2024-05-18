7d020ae7f0119e6618e240a4460655523fcbfac2
ZagloSs <carlosdiezmendez@gmail.com>
2024-05-18 23:22:46 +0200

    intro added to gameplay

---
+++ Assets/Scripts/Menus/anim.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class anim : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject canvas;
+    [SerializeField] private GameObject intro;
+
+    private float timer = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.SetActive(false);
+        canvas.SetActive(false);
+        intro.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (intro)
+        {
+            if (timer > 2f && intro.GetComponent<SpriteRenderer>().color.r > 0)
+            {
+                intro.GetComponent<SpriteRenderer>().color = new Color((intro.GetComponent<SpriteRenderer>().color.r) - ((200 * Time.deltaTime)/255),
+                                                                       (intro.GetComponent<SpriteRenderer>().color.g) - ((200 * Time.deltaTime) / 255),
+                                                                       (intro.GetComponent<SpriteRenderer>().color.b) - ((200 * Time.deltaTime) / 255));
+             
+               
+
+            }else if(timer > 4f && intro.GetComponent<SpriteRenderer>().color.r <= 0)
+            {
+                player.SetActive(true) ;
+                canvas.SetActive(true) ;
+
+                intro.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, (intro.GetComponent<SpriteRenderer>().color.a) - ((200 * Time.deltaTime) / 255));
+                if(timer > 5f)
+                {
+                    intro.SetActive(false);
+                    timer = 0;
+                }
+            }
+           
+            timer += Time.deltaTime;
+        }
+    }
+}
