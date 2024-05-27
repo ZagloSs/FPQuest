0f4a97e36ba69aac3f659604ee542294e2b1c44f
ZagloSs <carlosdiezmendez@gmail.com>
2024-05-27 22:14:59 +0200

    fixed some problems

--- Assets/Scripts/Misc/creditManager.cs
+++ Assets/Scripts/Misc/creditManager.cs
@@ -12,6 +12,7 @@ public class creditManager : MonoBehaviour
     private bool isFaded = false;
     void Start()
     {
+        
         StartCoroutine(Credits());
         
     }
@@ -31,7 +32,7 @@ public class creditManager : MonoBehaviour
         credits.transform.DOMoveY(21f, 15f).SetEase(Ease.OutSine);
         yield return new WaitForSeconds(18f);
         isFaded = true;
-        yield return new WaitForSeconds(22f);
+        yield return new WaitForSeconds(4f);
         GameManager.instance.backToMenu();
     }
      
